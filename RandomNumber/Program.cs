5c72abd42d016ea815339af03dfc25f46fb1998a
Marjanno Evert <marjanno.evert@tktk.ee>
2024-08-26 22:34:04 +0300

    week 4 loops

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rnd = new();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for (int i = 0; i < 7; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum += myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($" random sum total: {randomSum}");
